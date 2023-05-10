b4e9036ea7f9975db2579fbfec9c15b1e178704c
ikako <dzvelaia.irakli@gmail.com>
2023-05-10 16:20:10 +0400

    change nvim config

--- nvim/lua/ik/lsp/init.lua
+++ nvim/lua/ik/lsp/init.lua
@@ -6,3 +6,5 @@ end
 require "ik.lsp.mason"
 require("ik.lsp.handlers").setup()
 require "ik.lsp.null-ls"
+require "ik.lsp.lspconfig"
+require "ik.lsp.lspsaga"
--- nvim/lua/ik/plugins.lua
+++ nvim/lua/ik/plugins.lua
@@ -3,16 +3,16 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
+  PACKER_BOOTSTRAP = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+  print("Installing packer close and reopen Neovim...")
+  vim.cmd([[packadd packer.nvim]])
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -26,21 +26,21 @@ vim.cmd([[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-	return
+  return
 end
 
 -- Have packer use a popup window
 packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded" })
+    end,
+  },
 })
 
 return packer.startup(function(use)
   use { "wbthomason/packer.nvim" } -- Have packer manage itself
-  use { "nvim-lua/plenary.nvim" } -- Useful lua functions used by lots of plugins
+  use { "nvim-lua/plenary.nvim" }  -- Useful lua functions used by lots of plugins
 
   use {
     'svrana/neosolarized.nvim',
@@ -48,7 +48,7 @@ return packer.startup(function(use)
   }
   use { 'nvim-lualine/lualine.nvim' }
 
---  use { "catppuccin/nvim", requires = { 'tjdevries/colorbuddy.nvim' }, as = "catppuccin" } -- colorscheme
+  --  use { "catppuccin/nvim", requires = { 'tjdevries/colorbuddy.nvim' }, as = "catppuccin" } -- colorscheme
   use { 'akinsho/nvim-bufferline.lua' }
 
   use { 'windwp/nvim-autopairs' } -- automatically close brackets
@@ -63,19 +63,20 @@ return packer.startup(function(use)
   use { "kyazdani42/nvim-tree.lua" }
 
   -- cmp (completion plugins)
-  use { "hrsh7th/nvim-cmp" } -- The completion plugin
-  use { "hrsh7th/cmp-buffer" } -- buffer completions
-  use { "hrsh7th/cmp-path" } -- path completions
-	use { "saadparwaiz1/cmp_luasnip" } -- snippet completions
-	use { "hrsh7th/cmp-nvim-lsp" }
-	use { "hrsh7th/cmp-nvim-lua" }
+  use { "hrsh7th/nvim-cmp" }         -- The completion plugin
+  use { "hrsh7th/cmp-buffer" }       -- buffer completions
+  use { "hrsh7th/cmp-path" }         -- path completions
+  use { "saadparwaiz1/cmp_luasnip" } -- snippet completions
+  use { "hrsh7th/cmp-nvim-lsp" }
+  use { "hrsh7th/cmp-nvim-lua" }
 
   -- Language Server Protocol
-  use { "neovim/nvim-lspconfig" } -- enable LSP
-  use { "williamboman/mason.nvim"} -- simple to use language server installer
+  use { "neovim/nvim-lspconfig" }           -- enable LSP
+  use { "williamboman/mason.nvim" }         -- simple to use language server installer
   use { "williamboman/mason-lspconfig.nvim" }
-	use { "jose-elias-alvarez/null-ls.nvim" } -- for formatters and linters
-  use {'glepnir/lspsaga.nvim' } -- LSP UIs
+  use { "jose-elias-alvarez/null-ls.nvim" } -- for formatters and linters
+  use { "MunifTanjim/prettier.nvim" }
+  use { 'glepnir/lspsaga.nvim' }            -- LSP UIs
   use { "RRethy/vim-illuminate" }
 
   use {
@@ -91,7 +92,7 @@ return packer.startup(function(use)
   use { 'dinhhuy258/git.nvim' } -- For git blame & browse
 
   -- snippets
-  use { "L3MON4D3/LuaSnip" } --snippet engine
+  use { "L3MON4D3/LuaSnip" }             --snippet engine
   use { "rafamadriz/friendly-snippets" } -- a bunch of snippets to use
 
 
